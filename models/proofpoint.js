6684ff947ebfeb7db9bac173ca22b874339dd41c
Mike Rioux <michael.rioux@mongodb.com>
2018-02-19 17:46:27 -0500

    Added model, view and route for the create PP page

---
+++ models/proofpoint.js
@@ -0,0 +1,41 @@
+var mongoose = require('mongoose');
+var bcrypt = require('bcryptjs');
+
+// ProofPoint Schema
+var ProofPointSchema = mongoose.Schema({
+	title: {
+		type: String,
+	},
+	industry: {
+		type: String
+	},
+	useCaseType: {
+		type: String
+	},
+	secondaryUseCaseType: {
+		type: String
+    },
+    whyMongo: {
+		type: Array
+    },
+    pbos: {
+		type: Array
+    },
+    quote: {
+		type: String
+    },
+    speakerNotes: {
+		type: String
+    },
+    internalOnly: {
+		type: Boolean
+	}
+});
+ 
+
+var ProofPoint = module.exports = mongoose.model('ProofPoint', ProofPointSchema);
+
+module.exports.createPP = function(newPP, callback){
+    console.log("creatingPP");
+    newPP.save(callback);
+}
