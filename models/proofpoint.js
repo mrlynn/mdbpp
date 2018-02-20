010dec592476695ac9afb5aeeec53153158da42f
Michael Lynn <merlynn@gmail.com>
2018-02-20 08:18:38 -0500

    Fix navigation

--- models/proofpoint.js
+++ models/proofpoint.js
@@ -3,59 +3,59 @@ var bcrypt = require('bcryptjs');
 
 // ProofPoint Schema
 var ProofPointSchema = mongoose.Schema({
-	title: {
-		type: String,
-	},
-	industry: {
-		type: String
-	},
-	useCaseType: {
-		type: String
-	},
-	secondaryUseCaseType: {
-		type: String
-    },
-    whyMongo: {
-		type: Array
-    },
-    pbos: {
-		type: Array
-    },
-    companyName: {
-        type: String
-    },
-    quote: {
-		type: String
-    },
-    speakerNotes: {
-		type: String
-    },
-    internalOnly: {
-		type: Boolean
-    },
-    createdDate: {
-        type: Date
-    },
-    user: {
-      type: mongoose.Schema.Types.Mixed
-    },
-    logo: {
-        data: Buffer, contentType: String 
-    }
-    
+  title: {
+    type: String,
+  },
+  industry: {
+    type: String
+  },
+  useCaseType: {
+    type: String
+  },
+  secondaryUseCaseType: {
+    type: String
+  },
+  whyMongo: {
+    type: Array
+  },
+  pbos: {
+    type: Array
+  },
+  companyName: {
+    type: String
+  },
+  quote: {
+    type: String
+  },
+  speakerNotes: {
+    type: String
+  },
+  internalOnly: {
+    type: Boolean
+  },
+  createdDate: {
+    type: Date
+  },
+  user: {
+    type: mongoose.Schema.Types.Mixed
+  },
+  logo: {
+    data: Buffer, contentType: String
+  }
+
 });
- 
+
 
 var ProofPoint = module.exports = mongoose.model('ProofPoint', ProofPointSchema);
 
-module.exports.createPP = function(newPP, callback){
-    console.log("creatingPP");
-    newPP.save(callback);
+module.exports.createPP = function (newPP, callback) {
+  console.log("creatingPP");
+  newPP.save(callback);
 }
 
-module.exports.getPPByCompanyName = function(companyName, callback){
-    var query = {companyName: companyName};
-    ProofPoint.find(query,callback);
-	
+module.exports.getPPByCompanyName = function (companyName, callback) {
+  var query = { companyName: companyName };
+  ProofPoint.find(query, callback);
+
 }
 
--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,17 @@
 var express = require('express');
 var router = express.Router();
+var ProofPoint = require('../models/proofpoint');
 
 // Get Homepage
 router.get('/', ensureAuthenticated, function(req, res){
-	res.render('index',{
-		user: req.user
+	ProofPoint.find({},function(err,docs) {
+		console.log(JSON.stringify(docs));
+		res.render('index',{
+			user: req.user,
+			pp: docs
+		});
 	});
+
 });
 
 function ensureAuthenticated(req, res, next){
